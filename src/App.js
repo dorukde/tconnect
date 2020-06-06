addfa6e8bdd3bb15dd8f3c2ddf3d97bf47ead73b
Shams mosowi <shams.mosowi@gmail.com>
2020-06-06 14:07:57 +0800

    extrusions

--- src/App.js
+++ src/App.js
@@ -17,35 +17,27 @@ function Plane({ color, ...props }) {
 }
 
 function Extrusion({ start = [0,0], paths, ...props }) {
-  var length = 12, width = 8;
+
   const shape = useMemo(() => {
     const shape = new THREE.Shape()
-    shape.moveTo( 0,0 );
-    shape.lineTo( 10, 10 );
-    shape.lineTo( 20, 0 );
-   // shape.lineTo( length, 0 );
-    shape.lineTo( 0, 0 );
+    shape.moveTo( ...start );
+    paths.forEach(path=>{
+      shape.lineTo( ...path);
+    })
+    
     return shape
   }, [])
-  var extrudeSettings = {
-    steps: 2,
-    depth: 16,
-    bevelEnabled: true,
-    bevelThickness: 1,
-    bevelSize: 1,
-    bevelOffset: 0,
-    bevelSegments: 1
-  };
+
   return (
     <mesh>
-      <extrudeGeometry attach="geometry" args={[shape, extrudeSettings]} />
+      <extrudeGeometry attach="geometry" args={[shape, props]} />
       <meshBasicMaterial color="#0f0"/>
     </mesh>
   )
 }
 
 function App() {
-  var length = 12, width = 8;
+
   return (
     <Canvas concurrent shadowMap sRGB gl={{ alpha: false }} camera={{ position: [0, 0, 35] }}>
     <hemisphereLight intensity={0.35} />
@@ -53,13 +45,9 @@ function App() {
     <pointLight position={[-30, 0, -30]} intensity={0.5} />
     <Physics gravity={[0, 0, -30]}>
       <Plane color={niceColors[17][1]} />
-      <Plane color={niceColors[17][1]} position={[-6, 0, 0]} rotation={[0, 0.9, 0]} />
-      <Plane color={niceColors[17][2]} position={[6, 0, 0]} rotation={[0, -0.9, 0]} />
-      <Plane color={niceColors[17][3]} position={[0, 6, 0]} rotation={[0.9, 0, 0]} />
-      <Plane color={niceColors[17][0]} position={[0, -6, 0]} rotation={[-0.9, 0, 0]} /> 
-     
+ 
       <Suspense fallback={null}>
-      <Extrusion paths={[[0,width],[length,width],[length,length],[0,0]]}  amount={0}/>
+      <Extrusion start={[0,0]} paths={[[10,10],[20,0],[0,0]]} step={2} bevelEnabled depth={16} bevelSize={1}bevelThickness={1} bevelOffset={0} amount={0}/>
       </Suspense>
     </Physics>
   </Canvas>
